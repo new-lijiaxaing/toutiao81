93447b683a58f8e2504017d9aa862acaf4aa2397
new-lijiaxing <2191729114@qq.com>
2019-08-19 19:37:28 +0800

    头条PC-初始化项目-整理目录-准备资源

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
+
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
+import ElementUI from 'element-ui' // 引入UI
+import 'element-ui/lib/theme-chalk/index.css' // 引入样式
+import './style/index.less'// 引入重置的基础样式
 Vue.config.productionTip = false
-
+Vue.use(ElementUI)
 new Vue({
   router,
   render: h => h(App)
