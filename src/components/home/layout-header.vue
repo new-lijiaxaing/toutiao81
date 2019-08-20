9f2e54d431e257b7c677e6e9c942792b8508391b
new-lijiaxing <2191729114@qq.com>
2019-08-20 22:44:55 +0800

    头条PC-主页模块-头部信息查询

--- src/components/home/layout-header.vue
+++ src/components/home/layout-header.vue
@@ -6,12 +6,12 @@
         <span>中国持名品牌(管理)有限公司</span>
     </el-col>
     <el-col class="layout-right" :span="3">
-        <img src="../../assets/img/avatar.jpg" alt="">
+        <img :src="user.photo ? user.photo : defaultImg" alt="">
         <!-- 两个插槽 一个具名插槽 一个匿名插槽 -->
         <el-dropdown trigger="click">
             <!-- 匿名插槽 -->
             <span class="el-dropdown-link">
-                我是,你猜
+                {{user.name}}
             </span>
             <i class="el-icon-arrow-down el-icon--right"></i>
             <!-- 具名插槽 -->
@@ -26,8 +26,29 @@
 </template>
 
 <script>
-export default {
 
+export default {
+  data () {
+    return {
+      user: {
+
+      },
+      dafaultImg: require('../../assets/img/avatar.jpg')// require的图片就变成了base64
+    }
+  },
+  methods: {
+    // 获取用户信息
+    getUserInfo () {
+      let userInfo = window.localStorage.getItem('user-info')// 获取用户存储信息 存储信息里有token
+      let token = userInfo ? JSON.parse(userInfo).token : null // 获取token
+      token && this.$axios({
+        url: '/user/profile',
+        headers: { 'Authorization': `Bearer${token}` } // // 将headers中赋值 后端需要的token身份信息  携带令牌
+      }).then(result => {
+        this.user = result.data.data// 获取到用户的最新个人资料 赋值给data中的数据
+      })
+    }
+  }
 }
 </script>
 
