e85c1cb7ff7c88f9b2d647658e281f45c8f01dcc
new-lijiaxing <2191729114@qq.com>
2019-08-20 19:30:28 +0800

    头条PC-主页模块-主页布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>home</div>
+<!-- 最外层容器 -->
+  <el-container>
+    <!-- 左侧布局 -->
+    <el-aside>
+      侧边栏
+    </el-aside>
+
+    <!-- 右侧布局 -->
+  <el-container>
+    <!-- 头部区域 -->
+    <el-header>
+      头部区域
+    </el-header>
+    <!-- 中间区域 -->
+    <el-main>
+      右侧
+    </el-main>
+  </el-container>
+  </el-container>
 </template>
 
 <script>
